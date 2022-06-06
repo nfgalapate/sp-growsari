57b6f171269b8fffd3dd2475c6d53aff3f6725d2
nfgalapate <nicole.f.galapate@accenture.com>
2022-06-06 09:28:23 +0800

    added functionality to integrate with Random Dog Facts 3rd party api

--- sp-backend/routes/welcome.js
+++ sp-backend/routes/welcome.js
@@ -2,9 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+//const axios = require("axios").create({baseUrl: "http://dog-api.kinduff.com/api/facts"});
+const axios = require("axios");
+const currentUser = require("../middleware/currentuser");
 
-router.get("/", auth, (req, res) => {
-    res.status(200).send("Welcome 🙌 ");
+router.get("/", auth, async (req, res) => {
+    const response = await axios({
+        url: "http://dog-api.kinduff.com/api/facts",
+        method: "get"
+    });
+    res.status(200).send("Welcome " + currentUser(req,res) + "!" + "\n Here is a random dog fact for you: \n " + response.data.facts[0]);
 });
 
 module.exports = router;
